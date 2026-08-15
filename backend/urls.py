9f3c308fa38fee5da10172ea4a2c300a56e863db
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2026-08-15 18:30:29 +0530

    Fix catch-all routing to avoid intercepting api/admin/static URLs

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,7 +26,7 @@ urlpatterns = [
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
-    path('<path:path>', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!api|admin|static|images|media).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
