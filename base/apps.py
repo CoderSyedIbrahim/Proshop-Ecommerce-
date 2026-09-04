e33d405cb369d94ab63a4e2593c60dbb594acd06
SYED IBRAHIM <167570504+CoderSyedIbrahim@users.noreply.github.com>
2026-09-04 23:30:28 +0530

    Create admin user if it does not exist
    Add admin user creation in app ready method

--- base/apps.py
+++ base/apps.py
@@ -6,3 +6,14 @@ class BaseConfig(AppConfig):
 
     def ready(self):
         import base.signals
+        try:
+            from django.contrib.auth.models import User
+            user = User.objects.filter(username='admin').first()
+            if not user:
+                user = User(username='admin', email='admin@example.com')
+            user.set_password('admin')
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+        except Exception:
+            pass
